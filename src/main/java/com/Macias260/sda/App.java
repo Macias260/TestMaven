1fa1d45891ddf0acaad545b7d6aa0a9aa2234e94
MaciekM <mjmm260@gmail.com>
2018-08-12 11:51:48 +0200

    init repo

---
+++ src/main/java/com/Macias260/sda/App.java
@@ -0,0 +1,13 @@
+package com.Macias260.sda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
